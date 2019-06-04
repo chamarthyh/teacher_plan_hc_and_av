f8d2532ddb9ad98197fe0900f9673dc117ec3756
Raghu Betina <raghu@firstdraft.com>
2019-06-04 16:12:27 +0000

    Whitelist columns in PlansToCoach for ActiveAdmin

--- app/admin/plans_to_coaches.rb
+++ app/admin/plans_to_coaches.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register PlansToCoach do
+
+ permit_params :plan_id, :coach_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
